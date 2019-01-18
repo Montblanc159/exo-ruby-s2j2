24cdaa3a775d408e72e0595eb6e65045b33aed79
Montblanc159 <guillaume.zaugg@gmail.com>
2019-01-18 19:40:39 +0100

    completed 02

---
+++ lib/02_jean_mich_trade.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+def index_of_max(array)
+  array.each_with_index.max[1]
+end
+
+def day_trader(price_per_day)
+  array = price_per_day
+  comparisons = []
+  data = []
+
+  best_prices = []
+  array.each_with_index do |price, index|
+    comparison = array[index..array.length]
+    comparison.map! { |e| e - price }
+    data.push(comparison)
+  end
+
+  highest = []
+  data.each do |subed_price|
+    concat_prices_max = subed_price.max
+    highest.push(concat_prices_max)
+  end
+
+  best_prices.push(index_of_max(highest))
+  best_prices.push(array.find_index(highest.max + array[index_of_max(highest)]))
+
+  best_prices
+end
+
+puts day_trader([17, 3, 6, 9, 15, 8, 6, 1, 10])
---
+++ spec/02_jean_mich_trad_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative '../lib/02_jean_mich_trad.rb'
+
+describe "it decides of the best days to buy and sell actions" do
+  it "should return [1, 4]" do
+    expect(day_trader([17, 3, 6, 9, 15, 8, 6, 1, 10])).to eq([1, 4])
+  end
+end
